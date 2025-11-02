d345be4cc5a78de3e00f096e0ce44ee0d08b98c9
MateusCorrea09 <mateus.contato09@outlook.com>
2025-11-02 07:05:38 -0300

    resolvendo dois exercicios a+

---
+++ beecrowd1043/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd1043
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //para resolver esse exercicio eu preciso entender oq faz um triangulo ser um triangulo
+            // é baseado na soma de duas entradas especificas resultarem em um valor maior que a da 
+            // terceira entrada e assim  por diante! vou tentar aplicar isso aki :D
+
+            string[] entradas = Console.ReadLine().Split(' ');
+
+            double a = double.Parse(entradas[0]);
+            double b = double.Parse(entradas[1]);
+            double c = double.Parse(entradas[2]);
+
+            // sequência de checagens 
+            if (a + b > c)
+            {
+                if (a + c > b)
+                {
+                    if (b + c > a)
+                    {
+                        double resultado = a + b + c;
+                        Console.WriteLine($"Perimetro = {resultado:F1}");
+                    }
+                    else
+                    {
+                        double resultado = ((a + b) * c) / 2;
+                        Console.WriteLine($"Area = {resultado:F1}");
+                    }
+                }
+                else
+                {
+                    double resultado = ((a + b)* c) / 2;
+                    Console.WriteLine($"Area = {resultado:F1}");
+                }
+
+            }
+            else
+            {
+                double resultado = ((a + b) * c) / 2;
+                Console.WriteLine($"Area = {resultado:F1}");
+            }
+        }
+    }
+}
---
+++ beecrowd1133/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beecrowd1133
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+           
+                // eu preciso criar um contador que começa em um ponto e vai até ele
+                int inicial = 0;
+                int final = 0;
+                if (x > y) { inicial = y; final = x; } else { inicial = x; final = y; }
+                //Console.WriteLine("inicio = {0} // final {1}",inicial,final);
+                // n da pra usar o % para achar o resto do valor... quando se refere a um valor restante em ponto e virgula
+            //tava falktando o +1! porque ele deve começar a contar depois do valor inicial!   
+            for (int i = inicial + 1; i < final; i++)
+                {
+                    if (i % 5 == 2 || i % 5 == 3)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            
+           
+            
+
+    
+        }
+    }
+}
