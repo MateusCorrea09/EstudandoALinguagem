0c83d5d85ace7556256bc5d2ce3105e6878b0937
MateusCorrea09 <mateus.contato09@outlook.com>
2025-11-04 19:52:07 -0300

    mais um exercicio

--- beecrowd1045/Program.cs
+++ beecrowd1045/Program.cs
@@ -1,26 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace beecrowd1045
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        string[] entrada = Console.ReadLine().Split(' ');
+        double a = double.Parse(entrada[0]);
+        double b = double.Parse(entrada[1]);
+        double c = double.Parse(entrada[2]);
+
+        double[] lados = { a, b, c };
+        Array.Sort(lados);
+        Array.Reverse(lados);
+
+        a = lados[0];
+        b = lados[1];
+        c = lados[2];
+        if (a >= b + c)
         {
-            string[] entrada = Console.ReadLine().Split(' ');
-            double a = double.Parse(entrada[0]);
-            double b = double.Parse(entrada[1]);
-            double c = double.Parse(entrada[2]);
-            if (a == 0 || b == 0 || c == 0) { }
-            else if (a == b && b == a && a == c) { Console.WriteLine("TRIANGULO EQUILATERO"); }
-            else if (a == b || a == c || c == b) { Console.WriteLine("TRIANGULO ISOSCELES"); }
-            else if (a >= b + c) { Console.WriteLine("NAO FORMA TRIANGULO"); }
-            else if (Math.Pow(a, 2) == Math.Pow(b, 2) + Math.Pow(c, 2)) { Console.WriteLine("TRIANGULO RETANGULO"); }
-            else if (Math.Pow(a, 2) >= Math.Pow(b, 2) + Math.Pow(c, 2)) { Console.WriteLine("TRIANGULO OBTUSANGULO"); }
-            else if (Math.Pow(a, 2) < Math.Pow(b, 2) + Math.Pow(c, 2)) { Console.WriteLine("TRIANGULO ACUTANGULO"); }
+            Console.WriteLine("NAO FORMA TRIANGULO");
+        }
+        else
+        {
+            //Tem essa questão com testagens de if, verificação de mais de um 
+            //if no código, n entendi isso muito bem vou pegar pra ver isso com mais
+            //calma depois
+            if (Math.Pow(a, 2) == Math.Pow(b, 2) + Math.Pow(c, 2))
+                Console.WriteLine("TRIANGULO RETANGULO");
+            else if (Math.Pow(a, 2) > Math.Pow(b, 2) + Math.Pow(c, 2))
+                Console.WriteLine("TRIANGULO OBTUSANGULO");
+            else
+                Console.WriteLine("TRIANGULO ACUTANGULO");
+
+            if (a == b && b == c)
+                Console.WriteLine("TRIANGULO EQUILATERO");
+            else if (a == b || a == c || b == c)
+                Console.WriteLine("TRIANGULO ISOSCELES");
         }
     }
 }
