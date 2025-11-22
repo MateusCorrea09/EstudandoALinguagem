030305120e1bad2fefae4309c260452a15f36c09
MateusCorrea09 <mateus.contato09@outlook.com>
2025-11-22 19:43:53 -0300

    Recursividade Potenciação

---
+++ RecursividadePotencia/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursividadePotencia
+{
+    internal class Program
+    {
+
+        //Retorna A elevado a B, quando B = 0  retorna 1 
+        //A = base b = expoente
+        public static int Potencia(int a, int b)
+        {
+            if (b == 1)
+            {
+                //Quando o número é elavado a 0 o resultado é sempre 1
+                return 1;
+            }
+            else //Enquanto b for maior que 0 e 1 ...
+            {
+                return a * Potencia(a, b - 1);// quebra em partes o problema e depois retorna cada resultado
+            }                   //Dessa forma n precisa usar uma biblioteca para resolver esse problema
+        }
+        static void Main(string[] args)
+        {
+
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Potencia(a,b));
+            
+        }
+        
+    }
+}
---
+++ RecursividadePotencia/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RecursividadePotencia")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RecursividadePotencia")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a36649f0-264b-4ceb-86e7-976d70f9e15d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
